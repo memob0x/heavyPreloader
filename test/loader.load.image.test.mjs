8292206bf8bbcab26d0372d0a0385e25334306fc
Daniele Fioroni <dfioroni91@gmail.com>
2019-11-04 20:46:17 +0100

    image loader unit test enhancement

--- test/loader.load.image.test.mjs
+++ test/loader.load.image.test.mjs
@@ -1,13 +1,18 @@
 import { loadImage } from "../src/loader.load.image.mjs";
 
-const IMAGE_URL = "/base/test/resources/image.jpg";
-
 describe("Image loader", () => {
+    const IMAGE_URL = "/base/test/resources/image.jpg";
+
     it("should successfully load an image", () =>
         loadImage(IMAGE_URL).then(url => expect(url).to.equal(IMAGE_URL)));
 
     it("should throw an error when attempting to load an unreachable image", () =>
-        loadImage("//placehold.it/error").catch(error =>
+        loadImage("https://github.com/404").catch(error =>
+            expect(error).to.be.an.instanceOf(Error)
+        ));
+
+    it("should throw an error when attempting to load a wrong file type", () =>
+        loadImage("/base/test/resources/script.js").catch(error =>
             expect(error).to.be.an.instanceOf(Error)
         ));
 });
