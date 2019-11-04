39a35e3f4b8a6d39038cb3778d03ff0e8da53f13
Daniele Fioroni <dfioroni91@gmail.com>
2019-11-04 20:39:35 +0100

    image loader unit test

--- test/loader.load.image.test.mjs
+++ test/loader.load.image.test.mjs
@@ -1,8 +1,13 @@
 import { loadImage } from "../src/loader.load.image.mjs";
 
+const IMAGE_URL = "/base/test/resources/image.jpg";
+
 describe("Image loader", () => {
-    it("whatev", done => {
-        // TODO:
-        done();
-    });
+    it("should successfully load an image", () =>
+        loadImage(IMAGE_URL).then(url => expect(url).to.equal(IMAGE_URL)));
+
+    it("should throw an error when attempting to load an unreachable image", () =>
+        loadImage("//placehold.it/error").catch(error =>
+            expect(error).to.be.an.instanceOf(Error)
+        ));
 });
