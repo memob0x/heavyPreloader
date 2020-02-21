c6826d0a8ffefadd0622c1e877b64b5830a58704
Daniele Fioroni <dfioroni91@gmail.com>
2020-02-21 23:16:34 +0100

    tests build fail fix

--- test/loader.loaders.mjs
+++ test/loader.loaders.mjs
@@ -1,10 +1,4 @@
-import {
-    loadImage,
-    loadStyle,
-    loadObject,
-    loadScript
-} from "../src/loader.loaders.mjs";
-import Loaders from "../src/loader.loaders.mjs";
+import { loadImage, loadStyle, loadScript } from "../src/loader.loaders.mjs";
 
 describe("loaders", () => {
     it("should test loaders", done => done());
