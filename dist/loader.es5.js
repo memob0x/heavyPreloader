010e7ed50424c314bd3de20f1d74d524e50ac60f
Daniele Fioroni <dfioroni91@gmail.com>
2020-04-04 18:33:45 +0200

    package json security updates

--- dist/loader.es5.js
+++ dist/loader.es5.js
@@ -6,23 +6,15 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
-
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
-
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
@@ -31,163 +23,24 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 })(void 0, function () {
   'use strict';
 
-  var prop = function prop(o, p) {
-    return p.split(".").reduce(function (xs, x) {
-      return xs && xs[x] ? xs[x] : null;
-    }, o);
-  };
-
-  var getURL = function getURL(arg) {
-    arg = prop(arg, "url") || arg;
-    arg = prop(arg, "href") || arg;
-    var a = document.createElement("a");
-    a.href = arg;
-    return new URL(a);
-  };
-
-  var isSupportedElement = function isSupportedElement(el) {
-    return el instanceof HTMLImageElement || el instanceof HTMLPictureElement || el instanceof HTMLSourceElement || el instanceof HTMLVideoElement || el instanceof HTMLAudioElement;
+  var getURL = function getURL(href) {
+    return new URL(function (a) {
+      a.href = href;
+      return a;
+    }(document.createElement("a")));
   };
 
   var createWorker = function createWorker(work) {
-    work = typeof work !== "function" ? function () {} : work;
+    if (typeof work !== "function") {
+      throw new TypeError("Invalid argment of type ".concat(_typeof(work), " passed to Loader class internal \"createWorker\" function."));
+    }
+
     var url = URL.createObjectURL(new Blob(["(", work.toString(), ")()"], {
       type: "application/javascript"
     }));
     var worker = new Worker(url);
     URL.revokeObjectURL(url);
     return worker;
-  };
-
-  var getLoaderType = function getLoaderType(arg) {
-    arg = getURL(arg).href.split(/\#|\?/)[0].split(".").pop().trim();
-
-    switch (arg) {
-      case "jpg":
-      case "jpe":
-      case "jpeg":
-      case "jif":
-      case "jfi":
-      case "jfif":
-      case "gif":
-      case "tif":
-      case "tiff":
-      case "bmp":
-      case "dib":
-      case "webp":
-      case "ico":
-      case "cur":
-      case "svg":
-      case "png":
-        return "image";
-
-      case "css":
-        return "style";
-
-      case "js":
-      case "mjs":
-        return "script";
-
-      default:
-        return null;
-    }
-  };
-
-  var collection = new WeakMap();
-
-  var build = function build(data) {
-    var o = {};
-
-    if (isSupportedElement(data)) {
-      o.el = data;
-      o.url = getURL(prop(o.el, "dataset.src"));
-    } else {
-      o.el = prop(data, "el");
-      o.url = data instanceof URL ? data : prop(data, "url");
-    }
-
-    o.blob = prop(data, "blob");
-    return o;
-  };
-
-  var LoaderResource = function () {
-    function LoaderResource(data, override) {
-      _classCallCheck(this, LoaderResource);
-
-      var o = build(data);
-
-      if (collection.has(o.url) && !override) {
-        o = collection.get(o.url);
-      }
-
-      if (collection.has(o.el) && !override) {
-        o = collection.get(o.el);
-      }
-
-      this.el = o.el;
-      this.url = o.url;
-      this.blob = o.blob;
-      collection.set(o.el || o.url, this);
-    }
-
-    _createClass(LoaderResource, null, [{
-      key: "isLoaderResource",
-      value: function isLoaderResource(data) {
-        return data instanceof this;
-      }
-    }]);
-
-    return LoaderResource;
-  }();
-
-  var loadImage = function loadImage(url) {
-    var el = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : new Image();
-    return new Promise(function (resolve, reject) {
-      el.onload = function () {
-        return resolve(el);
-      };
-
-      el.onerror = reject;
-      el.src = url;
-    });
-  };
-
-  var loadStyle = function loadStyle(url) {
-    var el = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document.createElement("div");
-    var sheet = new CSSStyleSheet();
-    var promise = sheet.replace("@import url(\"".concat(url, "\")"));
-
-    if ("adoptedStyleSheets" in el) {
-      el.adoptedStyleSheets = [].concat(_toConsumableArray(el.adoptedStyleSheets), [sheet]);
-    }
-
-    return promise;
-  };
-
-  var loadScript = function loadScript(url) {
-    var el = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document.createElement("script");
-    return new Promise(function (resolve, reject) {
-      el.onload = function () {
-        return resolve(el);
-      };
-
-      el.onerror = reject;
-      el.src = url;
-      el.async = true;
-      document.head.append(el);
-    });
-  };
-
-  var Loaders = {
-    image: function image(url, el) {
-      return loadImage(url, el);
-    },
-    script: function script(url) {
-      return loadScript(url);
-    },
-    style: function style(url) {
-      return loadStyle(url, document);
-    }
   };
 
   var work = function work() {
@@ -246,20 +99,15 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
   var worker = null;
 
-  var loaderWorker = function loaderWorker() {
-    if (worker) {
-      return worker;
-    }
-
-    return worker = createWorker(work);
+  var getOrCreateWorker = function getOrCreateWorker() {
+    return worker ? worker : worker = createWorker(work);
   };
 
-  var collection$1 = {};
+  var cache = {};
 
-  var _fetch = function () {
-    var _ref2 = _asyncToGenerator(regeneratorRuntime.mark(function _callee2(resource) {
+  var fetch$1 = function () {
+    var _ref2 = _asyncToGenerator(regeneratorRuntime.mark(function _callee2(href) {
       var options,
-          el,
           _args2 = arguments;
       return regeneratorRuntime.wrap(function _callee2$(_context2) {
         while (1) {
@@ -267,48 +115,41 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
             case 0:
               options = _args2.length > 1 && _args2[1] !== undefined ? _args2[1] : {};
 
-              if (!(resource.url.href in collection$1)) {
-                _context2.next = 7;
+              if (!(href in cache)) {
+                _context2.next = 5;
                 break;
               }
 
-              el = resource.el;
-              _context2.next = 5;
-              return collection$1[resource.url.href];
+              _context2.next = 4;
+              return cache[href];
+
+            case 4:
+              return _context2.abrupt("return", _context2.sent);
 
             case 5:
-              resource = _context2.sent;
-              return _context2.abrupt("return", new LoaderResource(_objectSpread({}, resource, {}, {
-                el: el
-              }), true));
-
-            case 7:
-              return _context2.abrupt("return", collection$1[resource.url.href] = new Promise(function (resolve, reject) {
-                var worker = loaderWorker();
+              return _context2.abrupt("return", cache[href] = new Promise(function (resolve, reject) {
+                var worker = getOrCreateWorker();
                 worker.postMessage({
-                  href: resource.url.href,
+                  href: href,
                   options: options.fetch
                 });
                 worker.addEventListener("message", function (event) {
                   var data = event.data;
 
-                  if (data.href !== resource.url.href) {
+                  if (data.href !== href) {
                     return;
                   }
 
                   if (data.status === 200) {
-                    resource = new LoaderResource(_objectSpread({}, resource, {}, {
-                      blob: data.blob
-                    }), true);
-                    resolve(resource);
+                    resolve(data.blob);
                     return;
                   }
 
-                  reject(new Error("".concat(data.statusText, " ").concat(data.href)));
+                  reject(new Error("".concat(data.statusText, " for ").concat(data.href, " resource.")));
                 });
               }));
 
-            case 8:
+            case 6:
             case "end":
               return _context2.stop();
           }
@@ -316,63 +157,8 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
       }, _callee2);
     }));
 
-    return function _fetch(_x2) {
+    return function fetch$1(_x2) {
       return _ref2.apply(this, arguments);
-    };
-  }();
-
-  var _load = function () {
-    var _ref3 = _asyncToGenerator(regeneratorRuntime.mark(function _callee3(resource, options) {
-      var el, type, url;
-      return regeneratorRuntime.wrap(function _callee3$(_context3) {
-        while (1) {
-          switch (_context3.prev = _context3.next) {
-            case 0:
-              el = resource.el;
-              _context3.prev = 1;
-              _context3.next = 4;
-              return _fetch(resource, options);
-
-            case 4:
-              resource = _context3.sent;
-              resource.el = el;
-              _context3.next = 10;
-              break;
-
-            case 8:
-              _context3.prev = 8;
-              _context3.t0 = _context3["catch"](1);
-
-            case 10:
-              type = getLoaderType(resource);
-
-              if (type in Loaders) {
-                _context3.next = 13;
-                break;
-              }
-
-              throw new Error("invalid type");
-
-            case 13:
-              url = !!resource.blob ? URL.createObjectURL(resource.blob) : resource.url.href;
-              _context3.next = 16;
-              return Loaders[type](url, resource.el)["finally"](function () {
-                return URL.revokeObjectURL(url);
-              });
-
-            case 16:
-              return _context3.abrupt("return", resource);
-
-            case 17:
-            case "end":
-              return _context3.stop();
-          }
-        }
-      }, _callee3, null, [[1, 8]]);
-    }));
-
-    return function _load(_x3, _x4) {
-      return _ref3.apply(this, arguments);
     };
   }();
 
@@ -392,10 +178,6 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
       value: function fetch(arg) {
         var _this = this;
 
-        if (arg instanceof NodeList) {
-          return this.fetch(_toConsumableArray(arg));
-        }
-
         if (Array.isArray(arg)) {
           return arg.map(function (a) {
             return _this.fetch(a);
@@ -406,44 +188,11 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
           return this.fetch(getURL(arg));
         }
 
-        if (isSupportedElement(arg) || arg instanceof URL) {
-          return this.fetch(new LoaderResource(arg));
+        if (arg instanceof URL) {
+          return fetch$1(arg.href, this.options);
         }
 
-        if (LoaderResource.isLoaderResource(arg)) {
-          return _fetch(arg, this.options);
-        }
-
-        return Promise.reject(new Error("invalid argument"));
-      }
-    }, {
-      key: "load",
-      value: function load(arg) {
-        var _this2 = this;
-
-        if (arg instanceof NodeList) {
-          return this.load(_toConsumableArray(arg));
-        }
-
-        if (Array.isArray(arg)) {
-          return arg.map(function (a) {
-            return _this2.load(a);
-          });
-        }
-
-        if (typeof arg === "string") {
-          return this.load(getURL(arg));
-        }
-
-        if (isSupportedElement(arg) || arg instanceof URL) {
-          return this.load(new LoaderResource(arg));
-        }
-
-        if (LoaderResource.isLoaderResource(arg)) {
-          return _load(arg, this.options);
-        }
-
-        return Promise.reject(new Error("invalid argument"));
+        return Promise.reject(new TypeError("Invalid argment of type ".concat(_typeof(arg), " passed to Loader class \"fetch\" method.")));
       }
     }]);
 
--- dist/loader.js
+++ dist/loader.js
@@ -4,35 +4,25 @@
     (global = global || self, global.Loader = factory());
 }(this, (function () { 'use strict';
 
-    const prop = (o, p) =>
-        p.split(".").reduce((xs, x) => (xs && xs[x] ? xs[x] : null), o);
+    const getURL = (href) =>
+        new URL(
+            ((a) => {
+                a.href = href;
 
-    const getURL = arg => {
-        arg = prop(arg, "url") || arg;
-        arg = prop(arg, "href") || arg;
-
-        const a = document.createElement("a");
-        a.href = arg;
-
-        return new URL(a);
-    };
-
-    const isSupportedElement = el => {
-        return (
-            el instanceof HTMLImageElement ||
-            el instanceof HTMLPictureElement ||
-            el instanceof HTMLSourceElement ||
-            el instanceof HTMLVideoElement ||
-            el instanceof HTMLAudioElement
+                return a;
+            })(document.createElement("a"))
         );
-    };
 
-    const createWorker = work => {
-        work = typeof work !== "function" ? () => {} : work;
+    const createWorker = (work) => {
+        if (typeof work !== "function") {
+            throw new TypeError(
+                `Invalid argment of type ${typeof work} passed to Loader class internal "createWorker" function.`
+            );
+        }
 
         const url = URL.createObjectURL(
             new Blob(["(", work.toString(), ")()"], {
-                type: "application/javascript"
+                type: "application/javascript",
             })
         );
 
@@ -43,125 +33,8 @@
         return worker;
     };
 
-    const getLoaderType = arg => {
-        arg = getURL(arg)
-            .href.split(/\#|\?/)[0]
-            .split(".")
-            .pop()
-            .trim();
-
-        switch (arg) {
-            case "jpg":
-            case "jpe":
-            case "jpeg":
-            case "jif":
-            case "jfi":
-            case "jfif":
-            case "gif":
-            case "tif":
-            case "tiff":
-            case "bmp":
-            case "dib":
-            case "webp":
-            case "ico":
-            case "cur":
-            case "svg":
-            case "png":
-                return "image";
-            case "css":
-                return "style";
-            case "js":
-            case "mjs":
-                return "script";
-            default:
-                return null;
-        }
-    };
-
-    const collection = new WeakMap();
-
-    const build = data => {
-        let o = {};
-
-        if (isSupportedElement(data)) {
-            o.el = data;
-            o.url = getURL(prop(o.el, "dataset.src")); 
-        } else {
-            o.el = prop(data, "el");
-            o.url = data instanceof URL ? data : prop(data, "url");
-        }
-
-        o.blob = prop(data, "blob");
-
-        return o;
-    };
-
-    class LoaderResource {
-        constructor(data, override) {
-            let o = build(data);
-
-            if (collection.has(o.url) && !override) {
-                o = collection.get(o.url);
-            }
-
-            if (collection.has(o.el) && !override) {
-                o = collection.get(o.el);
-            }
-
-            this.el = o.el;
-            this.url = o.url;
-            this.blob = o.blob;
-
-            collection.set(o.el || o.url, this);
-        }
-
-        static isLoaderResource(data) {
-            return data instanceof this;
-        }
-    }
-
-    const loadImage = (url, el = new Image()) =>
-        new Promise((resolve, reject) => {
-            el.onload = () => resolve(el);
-            el.onerror = reject;
-
-            el.src = url;
-        });
-
-
-    const loadStyle = (url, el = document.createElement("div")) => {
-        const sheet = new CSSStyleSheet();
-
-        const promise = sheet.replace(`@import url("${url}")`);
-
-        if ("adoptedStyleSheets" in el) {
-            el.adoptedStyleSheets = [...el.adoptedStyleSheets, sheet];
-        }
-
-        return promise;
-    };
-
-
-    const loadScript = (url, el = document.createElement("script")) =>
-        new Promise((resolve, reject) => {
-            el.onload = () => resolve(el);
-            el.onerror = reject;
-
-            el.src = url;
-            el.async = true;
-
-            document.head.append(el);
-        });
-
-    var Loaders = {
-        image: (url,  el) => loadImage(url,  el ),
-        script: (url ) =>
- loadScript(url) ,
-        style: (url ) => loadStyle(url,  document )
-    };
-
     const work = () => {
-        onmessage = async event => {
+        onmessage = async (event) => {
             const data = event.data;
 
             let message;
@@ -172,12 +45,12 @@
                 message = {
                     status: response.status,
                     statusText: response.statusText,
-                    blob: blob
+                    blob: blob,
                 };
             } catch (e) {
                 message = {
                     status: 0,
-                    statusText: e
+                    statusText: e,
                 };
             }
 
@@ -187,134 +60,66 @@
     };
 
     let worker = null;
-    var loaderWorker = () => {
-        if (worker) {
-            return worker;
+
+    const getOrCreateWorker = () =>
+        worker ? worker : (worker = createWorker(work));
+
+    const cache = {};
+
+    var fetch$1 = async (href, options = {}) => {
+        if (href in cache) {
+            return await cache[href];
         }
 
-        return (worker = createWorker(work));
-    };
-
-    const collection$1 = {};
-
-    var _fetch = async (resource, options = {}) => {
-        if (resource.url.href in collection$1) {
-            const el = resource.el;
-
-            resource = await collection$1[resource.url.href];
-
-            return new LoaderResource({ ...resource, ...{ el: el } }, true);
-        }
-
-        return (collection$1[resource.url.href] = new Promise((resolve, reject) => {
-            const worker = loaderWorker();
+        return (cache[href] = new Promise((resolve, reject) => {
+            const worker = getOrCreateWorker();
 
             worker.postMessage({
-                href: resource.url.href,
-                options: options.fetch
+                href: href,
+                options: options.fetch,
             });
 
-            worker.addEventListener("message", event => {
+            worker.addEventListener("message", (event) => {
                 const data = event.data;
 
-                if (data.href !== resource.url.href) {
+                if (data.href !== href) {
                     return;
                 }
 
                 if (data.status === 200) {
-                    resource = new LoaderResource(
-                        { ...resource, ...{ blob: data.blob } },
-                        true
-                    );
-
-                    resolve(resource);
+                    resolve(data.blob);
 
                     return;
                 }
 
-                reject(new Error(`${data.statusText} ${data.href}`));
+                reject(new Error(`${data.statusText} for ${data.href} resource.`));
             });
         }));
     };
 
-    var _load = async (resource, options ) => {
-        const el = resource.el;
-
-        try {
-            resource = await _fetch(resource, options);
-            resource.el = el;
-        } catch (e) {}
-
-        const type = getLoaderType(resource);
-        if (!(type in Loaders)) {
-            throw new Error("invalid type");
-        }
-
-        const url = !!resource.blob
-            ? URL.createObjectURL(resource.blob)
-            : resource.url.href;
-
-        await Loaders[type](url,  resource.el).finally(() =>
-            URL.revokeObjectURL(url)
-        );
-
-        return resource;
-    };
-
     class Loader {
         constructor(options) {
-            this.options = {
-                ...{ fetch: { cors: "no-cors" } },
-                ...options
-            };
+            this.options = { ...{ fetch: { cors: "no-cors" } }, ...options };
         }
 
         fetch(arg) {
-            if (arg instanceof NodeList) {
-                return this.fetch([...arg]);
-            }
-
             if (Array.isArray(arg)) {
-                return arg.map(a => this.fetch(a));
+                return arg.map((a) => this.fetch(a));
             }
 
             if (typeof arg === "string") {
                 return this.fetch(getURL(arg));
             }
 
-            if (isSupportedElement(arg) || arg instanceof URL) {
-                return this.fetch(new LoaderResource(arg));
+            if (arg instanceof URL) {
+                return fetch$1(arg.href, this.options);
             }
 
-            if (LoaderResource.isLoaderResource(arg)) {
-                return _fetch(arg, this.options);
-            }
-
-            return Promise.reject(new Error("invalid argument"));
-        }
-
-        load(arg) {
-            if (arg instanceof NodeList) {
-                return this.load([...arg]);
-            }
-
-            if (Array.isArray(arg)) {
-                return arg.map(a => this.load(a));
-            }
-
-            if (typeof arg === "string") {
-                return this.load(getURL(arg));
-            }
-
-            if (isSupportedElement(arg) || arg instanceof URL) {
-                return this.load(new LoaderResource(arg));
-            }
-
-            if (LoaderResource.isLoaderResource(arg)) {
-                return _load(arg, this.options );
-            }
-
-            return Promise.reject(new Error("invalid argument"));
+            return Promise.reject(
+                new TypeError(
+                    `Invalid argment of type ${typeof arg} passed to Loader class "fetch" method.`
+                )
+            );
         }
     }
 
