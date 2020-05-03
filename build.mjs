d2ea453af82ac74209e9f1f24d0ca49f81fe7e44
Daniele Fioroni <dfioroni91@gmail.com>
2020-05-04 01:46:43 +0200

    build arguments

--- build.mjs
+++ build.mjs
@@ -1,5 +1,18 @@
-(async () =>
-    await Promise.all([
-        import("./build/tasks/library.mjs"),
-        import("./build/tasks/demo.mjs"),
-    ]))();
+let jobs = [];
+
+const args = process.argv;
+
+const libraryArg = args.indexOf("--library") > -1;
+const demoArg = args.indexOf("--demo") > -1;
+
+const noArgs = !libraryArg && !demoArg;
+
+if (libraryArg || noArgs) {
+    jobs.push(import("./build/tasks/library.mjs"));
+}
+
+if (demoArg || noArgs) {
+    jobs.push(import("./build/tasks/demo.mjs"));
+}
+
+(async () => await Promise.all(jobs))();
