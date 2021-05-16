45608ff2f9534012a1e01f448ee37844c164bce9
memob0x <dfioroni91@gmail.com>
2021-05-16 12:10:41 +0200

    chromium download env

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,23 +1,34 @@
-process.env.CHROME_BIN = './.local-chromium/chromium-linux-869685/chrome-linux/chrome';
+const downloadChromium = require('download-chromium');
 
-module.exports = config => config.set({
-    frameworks: ['mocha', 'chai'],
-    files: [
-        { pattern: './test/**/*.spec.js', type: 'module' },
-        { pattern: './src/**/*.js', type: 'module' },
-        {
-            pattern: './test/resources/*',
-            watched: false,
-            included: false,
-            served: true
-        }
-    ],
-    reporters: ['mocha'],
-    port: 9876, // karma web server port
-    colors: true,
-    logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
-    autoWatch: false,
-    // singleRun: false, // Karma captures browsers, runs the tests and exits
-    concurrency: Infinity
-});
+module.exports = async config => {
+    console.log('Downloading chromium...');
+
+    process.env.CHROME_BIN = await downloadChromium({
+        revision: '869685',
+        installPath: './.local-chromium' 
+    });
+    
+    console.log(`Chrome downloaded at ${process.env.CHROME_BIN}`);
+
+    config.set({
+        frameworks: ['mocha', 'chai'],
+        files: [
+            { pattern: './test/**/*.spec.js', type: 'module' },
+            { pattern: './src/**/*.js', type: 'module' },
+            {
+                pattern: './test/resources/*',
+                watched: false,
+                included: false,
+                served: true
+            }
+        ],
+        reporters: ['mocha'],
+        port: 9876, // karma web server port
+        colors: true,
+        logLevel: config.LOG_INFO,
+        browsers: ['ChromeHeadless'],
+        autoWatch: false,
+        // singleRun: false, // Karma captures browsers, runs the tests and exits
+        concurrency: Infinity
+    });
+};
